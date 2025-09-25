58212dc72e02798d52cf641c83b8726b05e798d4
GiuseppeTuccilli <giuseppetuccilli@gmail.com>
2025-09-25 16:46:34 +0200

    com9

--- src/main/java/giuseppetuccilli/dao/PartecipazioniDao.java
+++ src/main/java/giuseppetuccilli/dao/PartecipazioniDao.java
@@ -1,9 +1,13 @@
 package giuseppetuccilli.dao;
 
+import giuseppetuccilli.entities.Evento;
 import giuseppetuccilli.entities.Partecipazioni;
+import giuseppetuccilli.enums.Stato;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.TypedQuery;
 
+import java.util.List;
 import java.util.UUID;
 
 public class PartecipazioniDao {
@@ -35,6 +39,13 @@ public class PartecipazioniDao {
         em.remove(found);
         tr.commit();
         System.out.println("rimosso");
+    }
+
+    public List<Partecipazioni> getDaConfPerEvent(Evento ev) {
+        TypedQuery<Partecipazioni> query = em.createQuery("SELECT p FROM Partecipazione p WHERE p.evento= :ev AND p.stato = :nonC", Partecipazioni.class);
+        query.setParameter("evento", ev);
+        query.setParameter("nonC", Stato.NON_CONFERMATA);
+        return query.getResultList();
     }
 
 }
