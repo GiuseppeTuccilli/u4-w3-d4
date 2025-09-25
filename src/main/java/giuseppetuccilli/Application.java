46f4120573068ce3495f7ae4824b93c777713567
GiuseppeTuccilli <giuseppetuccilli@gmail.com>
2025-09-25 11:10:16 +0200

    Initial commit

---
+++ src/main/java/giuseppetuccilli/Application.java
@@ -0,0 +1,19 @@
+package giuseppetuccilli;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Application {
+    static EntityManagerFactory emf = Persistence.createEntityManagerFactory("u4-w3-d4pu");
+
+    public static void main(String[] args) {
+
+        EntityManager em = emf.createEntityManager();
+
+        
+        em.close();
+        emf.close();
+        System.out.println("Hello World!");
+    }
+}
